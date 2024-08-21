15145c90bf72db830c5b90e79b7536a1a0be53d4
Korkmaz <95500012+korkmazalper@users.noreply.github.com>
2024-08-21 16:01:49 +0200

    first commit

---
+++ php/000-intro/index.php
@@ -0,0 +1,23 @@
+<?php
+print "Hallo Welt!<br>";
+print "Hello world!";
+for($i=0;$i<5;$i++){
+    print("Hello W<br>");
+}
+
+$arr=array("Apfel","Birne","Banana");
+print_r($arr);
+echo "<br>";
+print(count($arr));
+
+print "<br><h1>Willkommen auf meiner Homepage!</h1>";
+
+$produkt = array(1, "Bohrmaschine", 45, "Kraftvolle Bohrmaschine für
+Bohr- und Schraubarbeiten", 23);
+print $produkt[0]."<br>\n";
+print $produkt[1]."<br>\n";
+print $produkt[2]."<br>\n";
+print $produkt[3]."<br>\n";
+print $produkt[4]."<br>\n";
+
+?>
